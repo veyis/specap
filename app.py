f05392701fbfe95e465234f95d9ea8fff1eb004a
veyis akgul <vakgul@gmail.com>
2023-04-28 13:08:20 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',  debug=True)
